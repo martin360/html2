38daa1913bdcbd896a096947434b75fe7be74874
martin360 <mr002903@gmail.com>
2016-10-26 08:37:17 -0700

    Add files via upload

---
+++ phpcode.html/Hello2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<?php
+    $color = "yellow";
+?>
+<body bgcolor="<?php echo "$color"; ?>";
+
+<?php
+$txt1 = "golf";
+$txt2 = "10.183.1.7";
+$txt3 = "hi";
+$x = 5;
+$y = 4;
+$z = 65521;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "my ip " . $txt2 . "<br>";
+echo "$txt3";
+echo $x + $y + $z;
+?>
+
+</body>
+</html>
